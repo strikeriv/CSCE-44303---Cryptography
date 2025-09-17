b37e3852690060041ed146fe74248929bcca303d
= <=>
2025-09-17 00:37:47 -0500

    i think we need child routes

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,12 @@ export default defineConfig({
       input: {
         main: 'src/index.html',
         hw1: 'src/hw1/index.html',
+        'hw1-encrypt': 'src/hw1/encrypt/index.html',
+        'hw1-decrypt': 'src/hw1/decrypt/index.html',
         hw3: 'src/hw3/index.html',
+        'hw3-part1': 'src/hw3/part1/index.html',
+        'hw3-part2': 'src/hw3/part2/index.html',
+        'hw3-part3': 'src/hw3/part3/index.html',
       },
     },
   },
